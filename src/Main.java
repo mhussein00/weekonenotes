0ef436d0b0702d0a1428559b5548405d851619a7
mhussein00 <148893874+mhussein00@users.noreply.github.com>
2024-01-23 12:31:54 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,15 @@ public class Main {
         double weather[] = {32, 25, 27, 40, 45};
         System.out.println("City: " + city);
         System.out.println("Zipcode: " + zipCode);
-        double sum = Arrays.stream(weather).sum()/weather.length;
-        System.out.println("Average Temperature: " + sum);
+        double sum = 0;
 
+        for(int i = 0; i < weather.length; i++) {
+            sum += weather[i]/weather.length;
+        }
+
+
+        System.out.println("Average Temperature: " + sum);
+        
     }
 }
 
